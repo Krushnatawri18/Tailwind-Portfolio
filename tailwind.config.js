714597430ff9988ff595e1f048fe4a85a2f97189
Krushna Tawri <tawrikrushna55@gmail.com>
2023-07-16 16:43:56 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "class",
+    content: ['./*.html'],
+    theme: {
+        extend: {
+            screens: {
+                "sm": "480px",
+                "md": "768px",
+                "lg": "1024px",
+                "xl": "1280px",
+            },
+            spacing: {
+                "big": "42rem"
+            }
+        },
+    },
+    plugins: [],
+}
+
